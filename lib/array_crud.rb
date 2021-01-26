23c6b1cbacb9d7fa96ee6ee0c836255f800b051d
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 22:33:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  return array[3]
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array.last
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
